4d8e0a3ed90b904bf4a3356726b0662a2d47953e
felsen <felsen88@gmail.com>
2018-01-16 15:38:45 +0545

    Modified: used dequeue for Palindrome check

--- dequeue.py
+++ dequeue.py
@@ -1,5 +1,6 @@
 """
 Dequeue Implementation.
+Double - Ended - Queue
 """
 
 class Dequeue:
@@ -17,26 +18,42 @@ class Dequeue:
         self.dequeue.append(item)
 
     def remove_rear(self,):
-        self.dequeue.pop(0)
+        return self.dequeue.pop(0)
 
     def remove_front(self,):
-        self.dequeue.pop()
+        return self.dequeue.pop()
         # self.dequeue.pop(-1)
 
     def size(self, ):
         return len(self.dequeue)
 
 
+# d = Dequeue()
+# d.add_rear(4)
+# d.add_rear('dog')
+# d.add_front('cat')
+# d.add_front(True)
+# d.size()
+# d.is_empty()
+# d.add_rear(8.4)
+# d.remove_rear()
+# d.remove_front()
 
-d = Dequeue()
-d.add_rear(4)
-d.add_rear('dog')
-d.add_front('cat')
-d.add_front(True)
-d.size()
-d.is_empty()
-d.add_rear(8.4)
-d.remove_rear()
-d.remove_front()
 
+def check_palindrome(a_number):
+    a = Dequeue()
+
+    for ch in a_number:
+        a.add_rear(ch)
+
+    still_equal = True
+    while a.size() > 1 and still_equal:
+        first = a.remove_front()
+        last = a.remove_rear()
+        if first != last:
+            still_equal = False
+    return still_equal
+                
+print(check_palindrome("sxdfcgvhbjnkml"))
+print(check_palindrome("madam"))
 
