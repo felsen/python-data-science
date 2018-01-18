c1fe9270497289e5bd0972d3dd0abc8cf377ce3f
felsen <felsen88@gmail.com>
2018-01-18 15:17:01 +0545

    Modified: changes added for add/search for Linked List Implementation

--- node.py
+++ node.py
@@ -34,6 +34,25 @@ class UnorderedList:
         temp.set_next(self.head)
         self.head = temp
 
+    def size(self, ):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+
 
 mylist = UnorderedList()
 mylist.add(10)
