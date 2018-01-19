7d4d1eee1973406f2b5629373a0dfc0ef44c2193
felsen <felsen88@gmail.com>
2018-01-19 10:25:39 +0545

    Modified: Sum of list number method 2

--- recursion.py
+++ recursion.py
@@ -9,3 +9,12 @@ def sum_of_list_1(num_list):
     return s
 
 print(sum_of_list_1([1,2,3,4,5,6,7,8,9]))
+
+
+def sum_of_list_2(num_list):
+    if len(num_list) == 1:
+        return num_list[0]
+    else:
+        return num_list[0] + sum_of_list_2(num_list[1:])
+
+print(sum_of_list_2([1,2,3,4,5,6,7,8,9]))
