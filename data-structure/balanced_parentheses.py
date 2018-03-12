b059290c7f34b606e4ad8805ee54619e945460f7
Felix Stephen <felsen88@gmail.com>
2018-03-12 17:07:06 +0530

    Modified: changes done for the Balanced Tree

--- data-structure/balanced_parentheses.py
+++ data-structure/balanced_parentheses.py
@@ -5,6 +5,7 @@ from stack import Stack
 
 __author__ = 'Felix Stephen'
 
+
 def balanced_parentheses(parentheses):
     """ Use a stack to check if a string of parentheses are balanced."""
     stack = Stack(len(parentheses))
@@ -16,26 +17,24 @@ def balanced_parentheses(parentheses):
     return not stack.is_empty()
 
 
+def balanced_para(paran):
+    a, b = [], []
+    for i in paran:
+        if i == "(":
+            a.append(i)
+        elif i == ")":
+            b.append(i)
+    if len(a) == len(b):
+        print("{} - True".format(paran))
+    else:
+        print("{} - False".format(paran))
+
+
 if __name__ == '__main__':
     examples = ['((()))', '((())']
     print('Balanced parentheses demonstration:\n')
     for example in examples:
         print(example + ': ' + str(balanced_parentheses(example)))
 
-
-#def balanced_para(paran):
-#    a, b = [], []
-#    for i in paran:
-#        if i == "(":
-#            a.append(i)
-#        elif i == ")":
-#            b.append(i)
-#    if len(a) == len(b):
-#        print("{} - True".format(paran))
-#    else:
-#        print("{} - False".format(paran))
-
-
-#if __name__ == '__main__':
-#    balanced_para("((()))))")
+    balanced_para("((()))))")
 
